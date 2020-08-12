7d0d29c800fe693dbf7bf5fb9f5ba84d655a2ce2
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-08-13 03:19:14 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $user = new App\User;
+        $user->user_id_card = "123456789";
+        $user->name = "Mister User";
+        $user->email = "user@gmail.com";
+        $user->password = Hash::make('password');
+        $user->save();
+
+        $staff = new App\Staff;
+        $staff->staff_id_card = "987654321";
+        $staff->name = "Mister Staff";
+        $staff->email = "staff@gmail.com";
+        $staff->password = Hash::make('password');
+        $staff->save();
     }
 }
