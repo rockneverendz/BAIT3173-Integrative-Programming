fddae5be5152357419f38c9b1d17f98523f2a61c
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-08-16 02:54:54 +0800

    Update list.blade.php

--- resources/views/admin/meal/list.blade.php
+++ resources/views/admin/meal/list.blade.php
@@ -11,24 +11,39 @@
             </div>
             @endif
 
-            <table class="table table-hover table-striped">
-                <thead>
+            <div class="accordion mb-3" id="accordionExample">
+                <div class="card">
+                    <div class="card-header" id="headingThree">
+                        <div class="mb-0">
+                            <button class="btn btn-link p-0 border-0 bg-transparent stretched-link" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            {{ Auth::user()->name }}
+                            </button>
+                        </div>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                        <div class="card-body">
+                            {{ Auth::user()->description }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <table class="table table-hover border">
+                <thead class="thead-light">
                     <tr>
                         <th scope="col">Name</th>
-                        <th scope="col">Description</th>
                         <th scope="col">Price</th>
                         <th scope="col">Availability</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($meals as $meal)
                     <tr>
-                        <th scope="row">{{ $meal->name }}</th>
-                        <td>{{ $meal->description }}</td>
+                        <td scope="row">{{ $meal->name }}</td>
                         <td>{{ $meal->price }}</td>
                         <td>{{ $meal->availability == 1 ? 'Available' : 'Unavailable' }}</td>
                         <td><a href="{{ route('admin.meal.retrive', ['id' => $meal->id], false) }}">{{ __('Details') }}</a></td>
-                        
                     </tr>
                     @endforeach
                 </tbody>
