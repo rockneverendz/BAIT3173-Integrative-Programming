fb2eca10c0046c128ccfdce2b123a79564a2e97c
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-08-21 03:31:29 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('user.login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('user.home') }}">Home</a>
                     @else
                         <a href="{{ route('user.login') }}">Login</a>
 
